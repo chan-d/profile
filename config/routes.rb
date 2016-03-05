646b70536d444a18d0205a03b04871aa2e589c02
chan-d <chan.danny516@gmail.com>
2016-03-04 19:30:28 -0800

    added about me page contact and projects pages, routed and working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/', to: 'pages#home'
+  get '/profile', to: 'pages#about_me', as: 'about_me'
+  get '/projects', to: 'pages#projects', as: 'projects'
+  get '/contact', to: 'pages#contact', as: 'contact'
 
 end
